bef873b43d7138f03f60f555435176e66b6bf420
alberto-mm <alberto.martin.montero@gmail.com>
2022-11-06 11:43:29 +0100

    Creadas validaciones en el modelo

--- models/inmueble.model.js
+++ models/inmueble.model.js
@@ -1,13 +1,37 @@
 const { Schema, model } = require('mongoose');
 
 const inmuebleModel = new Schema({
-    piso: Number,
+    piso: {
+        type: Number,
+        required: [true, 'El piso es un campo requerido']
+    },
     letra: String,
-    extension: Number,
-    num_habitaciones: Number,
-    alquilado: Boolean,
-    nombre_propietario: String,
-    email_contacto: String
+    extension: {
+        type: Number,
+        required: [true, 'La extensión es un campo requerido']
+    },
+    num_habitaciones: {
+        type: Number,
+        required: [true, 'El número de habitaciones es un campo requerido']
+    },
+    alquilado: {
+        type: Boolean,
+        required: [true, 'Si el piso se encuentra en alquiler es un campo requerido']
+    },
+    nombre_propietario: {
+        type: String,
+        required: [true, 'El nombre del propietario es un campo requerido']
+    },
+    email_contacto: {
+        type: String,
+        validate: {
+            validator: (value) => {
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+            },
+            message: 'El email no es válido'
+        },
+        required: [true, 'El email es un campo requerido']
+    }
 });
 
 module.exports = model('inmueble', inmuebleModel, 'inmuebles');
